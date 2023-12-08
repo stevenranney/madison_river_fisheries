56e8552105f147b13f91ca52ccf6067061a0f0a7
Steven H. Ranney <steven.ranney@gmail.com>
2023-12-08 10:20:12 -0700

    lots of changes, including some new figures, updates to files, and more literature.

--- R/09_calculate_recycling_rates.R
+++ R/09_calculate_recycling_rates.R
@@ -35,14 +35,39 @@ pop_est_preds <-
   output %>%
   bind_rows()
 
-pop_est_preds
+pop_est_preds_img <- 
+  pop_est_preds %>%
+  ggplot() +
+  geom_line(aes(x = year, y = fit), linetype = 2) +
+  geom_ribbon(aes(x = year, ymin = lwr, ymax = upr), alpha = 0.5) +#,
+  # geom_smooth(aes(x = year, y = n_hat_km), linetype = 1, color = 'black', se = FALSE, level = 0.95, method = 'lm', formula = y~x) +
+  facet_rep_grid(location~sp) +
+  # lims(y = c(0, 30000)) +
+  xlab('Year') +
+  ylab(expression("Predicted population estimate ("~hat("N")~")/km")) +
+  theme_minimal(base_size = 30) +
+  theme(legend.position = 'bottom', 
+        legend.title=element_blank(),
+        panel.grid.minor = element_blank(),
+        # panel.border = element_rect(colour = "black", fill=NA, size=1)
+        # panel.border = element_blank(),
+        axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
+        axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black"),
+  )
+
+pop_est_preds_img
+
+ggsave(paste0("output/images/09_pop_est_preds.png"), plot = pop_est_preds_img, 
+       width = 16, height = 9, bg = "white")
 
 
-pred_catch <- read.csv('output/08_predicted_annual_catch_mortality.csv', header = T)
+pred_catch <- 
+  read.csv('output/08_predicted_annual_catch_mortality.csv', header = T)
 
 
 
-pop_est_preds %>%
+brown_recycling_rate <- 
+  pop_est_preds %>%
   filter(sp == 'Brown') %>%
   left_join(
     pred_catch %>%
@@ -55,3 +80,22 @@ pop_est_preds %>%
   mutate(re_lower = catch_lwr/fit, 
          re_fit = catch_fit/fit, 
          re_upr = catch_upr/fit)
+
+brown_recycling_rate
+
+rainbow_recycling_rate <- 
+  pop_est_preds %>%
+  filter(sp == 'Rainbow') %>%
+  left_join(
+    pred_catch %>%
+      select(year, rbt_catch_lwr, rbt_catch_fit, rbt_catch_upr) %>%
+      mutate(catch_lwr = rbt_catch_lwr/86.4, 
+             catch_fit = rbt_catch_fit/86.4, 
+             catch_upr = rbt_catch_upr/86.4) %>%
+      select(year, catch_lwr, catch_fit, catch_upr) 
+  ) %>%
+  mutate(re_lower = catch_lwr/fit, 
+         re_fit = catch_fit/fit, 
+         re_upr = catch_upr/fit)
+
+rainbow_recycling_rate
