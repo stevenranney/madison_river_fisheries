0b94417815daa9215829d6f353f3232abc996c04
Steven H. Ranney <steven.ranney@gmail.com>
2024-04-30 11:32:30 -0600

    MOAR CLEANUP!

--- R/07_madison_river_angler_days_predictions.R
+++ R/07_madison_river_angler_days_predictions.R
@@ -5,12 +5,6 @@ library(mgcv)
 library(scales)
 library(tidyr)
 
-# From 2020 survey: totals and summer
-# .58 is proportion of users who use "upper" river
-total_angler_days <- 77453
-summer_angler_days <- 58716
-
-summer_angler_days*.58
 
 # angler days on the upper madison river from Horton paper, 2018
 actual <- data_frame(
@@ -20,19 +14,22 @@ actual <- data_frame(
 
 actual
 
+# Generate lm from these data
 mod <- lm(angler_days~year, data = actual)
 summary(mod)
 
+# add predcited values to df
 actual <- cbind(actual, predict(mod, interval = 'confidence'))
 actual
 
-
+# Create a df to extrapolate
 future <- data.frame(
   year = seq(2016, 2040, by = 1), 
   angler_days = NA, 
   type = 'predicted')
 future
 
+# Generate predictions
 predicted = cbind(future, predict(mod, newdata = future, interval = 'prediction', level = 0.95))
 
 from_2020_pressure <- # from League and Caball 2022
@@ -43,14 +40,14 @@ from_2020_pressure <- # from League and Caball 2022
              lwr = NA, 
              upr = NA)
 
+# Create df of all years from 2011 through 2040
 all <- 
   actual %>%
   rbind(predicted) %>%
   mutate(angler_days = ifelse(year == 2020, 310762*.67, 
                               ifelse(year == 2019, 263735*.67, 
                                      ifelse(year == 2017, 306448*.67, angler_days))), 
-         )#, 
-         # type = ifelse(year == 2020, 'actual', type))
+         )
 
 all %>%
   saveRDS("./data/07_predicted_angler_pressure.rds")
@@ -63,7 +60,7 @@ p <-
   ggplot() +
   aes(x = year, y = fit) +
   geom_point((aes(x = year, y = angler_days)), size = 4) +
-  geom_line(aes(linetype = type), size = 1) +
+  geom_line(aes(linetype = type), linewidth = 1) +
   geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3) +
   xlab("Year") +
   ylab('N anglers/year\n(upper Madison River)') +
@@ -71,8 +68,8 @@ p <-
                           labels = c('Observed and 95% confidence interval',
                                      'Prediction and 95% prediction interval')
   ) + 
-  # scale_shape_manual(name = "test", 
-  #                    values = c(16, NA), 
+  # scale_shape_manual(name = "test",
+  #                    values = c(16, NA),
   #                    labels = c('Observed',
   #                               NA)
   # ) +
@@ -92,9 +89,9 @@ p
 ggsave(paste0("output/images/07_predicted_yearly_n_anglers.png"), plot = p, 
        width = 16, height = 9, bg = "white")
 
-# all %>% 
-#   filter(year >= 2023 & year <= 2030) %>% 
-#   select(year, fit, lwr, upr) %>% 
+# all %>%
+#   filter(year >= 2023 & year <= 2030) %>%
+#   select(year, fit, lwr, upr) %>%
 #   pivot_longer(!year, names_to = 'type', values_to = 'n_anglers') %>%
 #   mutate(n_upper_anglers = n_anglers * .67)
 # 
